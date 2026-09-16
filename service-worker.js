fc559793b2ac33152dc6fc727ed7a71e8f5257c7
StanVrazz <sub@morlach.de>
2026-09-16 11:11:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,22 @@
-const CACHE = "zeljezni-dnevnik-v5";
+const CACHE = "zeljezni-dnevnik-v9";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
-  "./images/leg-press.png",
+  "./images/assisted-pull-up.png",
   "./images/chest-press.png",
-  "./images/shoulder-press.png",
-  "./images/assisted-pull-up.png"
+  "./images/leg-press.png","./images/iron-cover.png","./images/iron-weight-texture.png",
+  "./images/shoulder-press.png"
 ];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./index.html"));
-    })
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response; }).catch(()=>caches.match("./index.html"))));
 });

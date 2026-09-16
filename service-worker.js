46718b5de17327923c9aa7ed66389e33a6ed6935
StanVrazz <sub@morlach.de>
2026-09-16 12:15:07 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "zeljezni-dnevnik-v10";
+const CACHE = "zeljezni-dnevnik-v11";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,7 +7,7 @@ const ASSETS = [
   "./icon-512.png",
   "./images/assisted-pull-up.png",
   "./images/chest-press.png",
-  "./images/leg-press.png","./images/iron-cover.png","./images/iron-weight-texture.png","./images/iron-weight-texture-hd.png","./images/iron-weight-texture-clean.png",
+  "./images/leg-press.png","./images/iron-cover.png","./images/iron-weight-texture.png","./images/iron-weight-texture-hd.png","./images/iron-weight-texture-clean.png","./images/iron-weight-texture-v11.png",
   "./images/shoulder-press.png"
 ];
 self.addEventListener("install", event => {

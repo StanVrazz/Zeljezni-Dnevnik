cfda3883d7d36de910f6835914cf7c2965022aaa
StanVrazz <sub@morlach.de>
2026-09-16 18:15:42 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "zeljezni-dnevnik-v14";
+const CACHE = "zeljezni-dnevnik-v15";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,16 +7,39 @@ const ASSETS = [
   "./icon-512.png",
   "./images/assisted-pull-up.png",
   "./images/chest-press.png",
-  "./images/leg-press.png","./images/iron-cover.png","./images/iron-cover-clean-v12.png ,"./images/iron-weight-texture-clean-v13.png"","./images/iron-weight-texture.png","./images/iron-weight-texture-hd.png","./images/iron-weight-texture-clean.png","./images/iron-weight-texture-v11.png",
-  "./images/shoulder-press.png"
+  "./images/leg-press.png",
+  "./images/shoulder-press.png",
+  "./images/iron-cover.png",
+  "./images/iron-cover-clean-v12.png",
+  "./images/iron-weight-texture-clean-v13.png",
+  "./images/iron-weight-texture.png",
+  "./images/iron-weight-texture-hd.png",
+  "./images/iron-weight-texture-clean.png",
+  "./images/iron-weight-texture-v11.png"
 ];
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response; }).catch(()=>caches.match("./index.html"))));
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(c => c.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
 });
